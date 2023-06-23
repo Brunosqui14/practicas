2cb7cbb6fc8b8f093913e5ef6b3478d3e142c33c
brunoquezada <brunoquezada41@gmail.com>
2023-06-23 13:35:23 -0300

    modificacion de path y creacion de lazyload para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"",loadChildren:()=>import("./modules/auth/auth.module").then(m=>m.AuthModule)}
+  {
+    path:"",loadChildren:()=>import("./modules/auth/auth.module").then(m=>m.AuthModule)
+  },
+  {
+    path:"",loadChildren:()=>import("./modules/home/home.module").then(m=>m.HomeModule)
+  }
 ];
 
 @NgModule({
